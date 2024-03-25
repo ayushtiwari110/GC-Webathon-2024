6480707214a6d4d962f0b5e712986a741cf1fab5
karthick <karthickcherukuri@gmail.com>
2024-03-25 20:22:13 +0530

    add admin dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,36 @@
-import './App.css'
-import { BrowserRouter as Router, useRoutes } from 'react-router-dom';
-import LandingPage from './pages/LandingPage';
-import StudentDashboard from './pages/StudentDashboard';
+import "./App.css";
+import { BrowserRouter as Router, useRoutes } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import StudentDashboard from "./pages/StudentDashboard";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import AdminDashBoard from "./pages/AdminDashBoard";
+
 const Routes = () => {
   const routes = useRoutes([
     {
-      path: '/',
-      element: <LandingPage/>
+      path: "/",
+      element: <LandingPage />,
     },
     {
-      path: '/student',
-      element: <StudentDashboard/>
-    }
+      path: "/student",
+      element: <StudentDashboard />,
+    },
+    {
+      path: "/admin",
+      element: <AdminDashBoard />,
+    },
   ]);
   return routes;
-}
+};
 
 function App() {
-
   return (
-    <Router>
-      <Routes />
-    </Router>
-  )
+    <GoogleOAuthProvider clientId="118125970358-hhhd7bta5n2649ca6mrjffa3bd990i7b.apps.googleusercontent.com">
+      <Router>
+        <Routes />
+      </Router>
+    </GoogleOAuthProvider>
+  );
 }
 
-export default App
+export default App;
