c9b35eb3e9bc2a81aa08b430c324ae8c7d275ed9
Ayush Tiwari <ayushtiwari110@yahoo.com>
2024-03-25 21:48:37 +0530

    add google oauth provider

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import StudentDashboard from './pages/StudentDashboard';
 import StudentProfile from './pages/StudentProfile';
 import CourseEnrollment from './pages/CourseEnrollment';
 import AdminDashBoard from './pages/AdminDashBoard'; // Added import for AdminDashBoard
-
+import { GoogleOAuthProvider } from '@react-oauth/google';
 const Routes = () => {
   const routes = useRoutes([
     {
@@ -34,9 +34,11 @@ const Routes = () => {
 
 function App() {
   return (
-    <Router>
-      <Routes />
-    </Router>
+    <GoogleOAuthProvider clientId="118125970358-hhhd7bta5n2649ca6mrjffa3bd990i7b.apps.googleusercontent.com">
+      <Router>
+        <Routes />
+      </Router>
+    </GoogleOAuthProvider>
   );
 }
 
